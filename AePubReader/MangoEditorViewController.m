693b1a7cfe944c4c1d5fb5e6661a6d3fded8e974
Kedar Kulkarni <kedarknk@gmail.com>
2013-10-18 11:11:58 +0530

    Editor - Added a 3rd party library for future use in editor

---
+++ AePubReader/MangoEditorViewController.m
@@ -0,0 +1,38 @@
+//
+//  MangoEditorViewController.m
+//  MangoReader
+//
+//  Created by Kedar Kulkarni on 15/10/13.
+//
+//
+
+#import "MangoEditorViewController.h"
+
+@interface MangoEditorViewController ()
+
+@end
+
+@implementation MangoEditorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
