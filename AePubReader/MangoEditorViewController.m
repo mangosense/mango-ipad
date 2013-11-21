d65576959fd422229a9309df7cc8fa8cdd15ff43
Kedar Kulkarni <kedarknk@gmail.com>
2013-11-21 23:13:35 +0530

    Editor - Issue in audio mapping text resolved

--- AePubReader/MangoEditorViewController.m
+++ AePubReader/MangoEditorViewController.m
@@ -1060,7 +1060,16 @@ enum
     NSString *recDir = [paths objectAtIndex:0];
     NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/sampleRecord_%d.caf", recDir, currentPageNumber]];
     
-    NSDictionary *pageDict = [pagesArray objectAtIndex:currentPageNumber];
+    NSDictionary *pageDict;
+    for (NSDictionary *readerPageDict in pagesArray) {
+        if ([[readerPageDict objectForKey:PAGE_NAME] isEqualToString:[NSString stringWithFormat:@"%d", currentPageNumber]]) {
+            pageDict = readerPageDict;
+            break;
+        } else if ([[readerPageDict objectForKey:PAGE_NAME] isEqualToString:@"Cover"] && currentPageNumber == 0) {
+            pageDict = readerPageDict;
+            break;
+        }
+    }
     NSArray *layersArray = [pageDict objectForKey:LAYERS];
     NSString *textOnPage;
     CGRect textFrame = CGRectMake(100, 100, 600, 400);
