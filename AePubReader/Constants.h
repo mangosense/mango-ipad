ef3becbcda309ea1cc9aa31d0d12a461a7a18382
Kedar Kulkarni <kedarknk@gmail.com>
2014-02-27 12:22:09 +0530

    General - Added some constants

--- AePubReader/Constants.h
+++ AePubReader/Constants.h
@@ -91,7 +91,7 @@
 
 #pragma mark - URL's
 
-#define BASE_URL @"http://192.168.2.28:3001/api/v2"
+#define BASE_URL @"http://api.mangoreader.com/api/v2"
 //api.mangoreader.com
 //testapi.mangoreader.com
 //192.168.2.28:3001/api/v2
