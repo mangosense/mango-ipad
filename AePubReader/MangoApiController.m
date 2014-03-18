355e14135da94f44c88225e2a4874ca176c3d308
HarishMangosense <harish@mangosense.com>
2014-03-18 17:44:50 +0530

    reachability handle all view

--- AePubReader/MangoApiController.m
+++ AePubReader/MangoApiController.m
@@ -118,6 +118,10 @@
         }
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Get List Error: %@", error);
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[error localizedDescription] delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [alert show];
+        
         if ([delegate respondsToSelector:@selector(reloadViewsWithArray:ForType:)]) {
             [delegate reloadViewsWithArray:[NSArray array] ForType:methodName];
         }
