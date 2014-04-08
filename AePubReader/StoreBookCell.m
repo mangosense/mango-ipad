16c8d86df468b2ff729d385139ef8e18e9db920c
HarishMangosense <harish@mangosense.com>
2014-04-08 22:25:12 +0530

    iPhone compatible reader push

--- AePubReader/StoreBookCell.m
+++ AePubReader/StoreBookCell.m
@@ -20,13 +20,30 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        _bookImageView = [[UIImageView alloc] initWithFrame:CGRectMake(2, 2, 127, 134)];
+        
+        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+            
+            _bookImageView = [[UIImageView alloc] initWithFrame:CGRectMake(2, 2, 100, 105)];
+            _frameImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 102, 107)];
+            _bookTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, _frameImageView.frame.size.height, 102, 16)];
+            _bookTitleLabel.font = [UIFont boldSystemFontOfSize:12];
+            
+        }
+        else{
+            _bookImageView = [[UIImageView alloc] initWithFrame:CGRectMake(2, 2, 127, 134)];
+            _frameImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 130, 137)];
+            _bookTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, _frameImageView.frame.size.height, 130, 20)];
+            _bookTitleLabel.font = [UIFont boldSystemFontOfSize:16];
+        }
+
+        
+       // _bookImageView = [[UIImageView alloc] initWithFrame:CGRectMake(2, 2, 127, 134)];
         [_bookImageView setContentMode:UIViewContentModeScaleToFill];
         [[_bookImageView layer] setCornerRadius:12.0f];
         [_bookImageView setClipsToBounds:YES];
         [self addSubview:_bookImageView];
         
-        _frameImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 130, 137)];
+      //  _frameImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 130, 137)];
         [_frameImageView setImage:[UIImage imageNamed:@"bookframe.png"]];
         [self addSubview:_frameImageView];
         
@@ -42,10 +59,10 @@
         [_buyBookButton.titleLabel setFont:[UIFont boldSystemFontOfSize:18]];
         [self addSubview:_buyBookButton];*/
         
-        _bookTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, _frameImageView.frame.size.height, 130, 20)];
+        //_bookTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, _frameImageView.frame.size.height, 130, 20)];
         _bookTitleLabel.numberOfLines = 2;
         _bookTitleLabel.textColor = COLOR_DARK_RED;
-        _bookTitleLabel.font = [UIFont boldSystemFontOfSize:16];
+     //   _bookTitleLabel.font = [UIFont boldSystemFontOfSize:16];
         [self addSubview:_bookTitleLabel];
         
         _bookPriceLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, _bookTitleLabel.frame.origin.y + _bookTitleLabel.frame.size.height, 130, 20)];
