f64c8d283339b40d7f327696c12842a7b87e0ab7
rameshv <ramesh@mangosense.com>
2014-04-10 21:48:02 +0530

    story packaging with proper images & bundle id & code signed

--- AePubReader/story_builder.rb
+++ AePubReader/story_builder.rb
@@ -2,10 +2,25 @@ require 'clockwork'
 require 'xcoder/rake_task'
 require 'net/http'
 require 'open-uri'
+require 'mini_magick'
 
-module Clockwork
-	handler do |job|
+#module Clockwork
+#	handler do |job|
    
+  		def create_image(size)
+			image = MiniMagick::Image.open 'cover.png'
+			image.resize  size
+			image.quality  100
+			image.format 'png'
+			image.write "mangoreader-app-icon-#{size}.png"
+			# gloss_image = MiniMagick::Image.open "gloss-images/gloss-#{size.split('x').first}.png"
+			# image = image.composite(gloss_image) do |c|
+			# 	c.compose 'over'
+			# 	c.geometry "+20+20"
+			# end
+			# image.write "mangoreader-app-icon-#{size}.png"
+		end
+
 	    puts "Starting story builder"
 
 	    #checking for stories available for ios export
@@ -16,6 +31,8 @@ module Clockwork
 			http.request req
 		}
 		story_ids = JSON.parse res.body
+
+		story_ids = [story_ids.sample]
 
 		story_ids.each do |story_id|
 		
@@ -57,7 +74,6 @@ module Clockwork
 
 			source_file = 'MangoStory.zip'
 
-
 			#get the config object of the selected target
 			config = target.config(:Debug)
 			config.product_name = "Mango Story App" #or title of the story
@@ -65,24 +81,41 @@ module Clockwork
 
 			#set the app info here
 			config.info_plist do |info|
-				info.version = info.version.to_i + 1
+				info.version = 1.0
 				info.display_name = story_info['title']
-				info.identifier = "com.mangosense.storyapp.#{story_info['id']}" #uniqe story id at last
+				info.identifier = "com.mangosense.MangoStory.#{story_info['id']}" #uniqe story id at last
 			end
+
+			#download the cover
+			cover_url = "http://mangoreader.com/#{story_info['cover']}"
+
+
+			File.open("cover.png", "wb") do |saved_file|
+			  # the following "open" is provided by open-uri
+			  open(cover_url, "rb") do |read_file|
+			    saved_file.write(read_file.read)
+			  end
+			end
+
+  			#create app icons using the cover image
+			['57x57','76x76','114x114','120x120','144x144','152x152'].each {|x| create_image x}
+
 
 			builder = config.builder
 			builder.clean
 			builder.build
-
+			break
 
 			#TODO
 			#1-Upload the app to app store using phantom
 			#2-Update the story app status to live
 		end
 
+
 	
-	end
 
-  every(10.minutes, 'story.job')
+	#end
 
-end
+#  every(10.minutes, 'story.job')
+#
+#end
