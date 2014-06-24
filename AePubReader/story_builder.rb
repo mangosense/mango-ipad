760766aa62cd8219f9e7a52a94b6335569f656e2
rameshv <ramesh@mangosense.com>
2014-06-24 18:52:38 +0530

    file utils & transporter fixes

--- AePubReader/story_builder.rb
+++ AePubReader/story_builder.rb
@@ -9,13 +9,12 @@ require 'FileUtils'
 #module Clockwork
 #	handler do |job|
    
-   		user_name = ARGV[0]
-   		password = ARGV[1]
-   		app_creation = ARGV[2] || 'no'
-   		app_upload = ARGV[3] || 'no'
-   		just_distribute = ARGV[4] || 'no'
-   		story_id = nil
-   		story_config = nil
+   		@user_name = ARGV[0]
+   		@password = ARGV[1]
+   		@app_creation = ARGV[2] || 'no'
+   		@just_distribute = ARGV[3] || 'no'
+   		@story_id = nil
+   		@story_config = nil
   		
   		def create_image(size)
 			image = MiniMagick::Image.open 'cover.png'
@@ -49,47 +48,47 @@ require 'FileUtils'
 
 		def create_json
 
-			puts "Story ID : #{story_id}" 
+			puts "Story ID : #{@story_id}" 
 
 			#get the story info
-			url = URI "http://api.mangoreader.com/api/v2/livestories/#{story_id}/info"
+			url = URI "http://api.mangoreader.com/api/v2/livestories/#{@story_id}/info"
 			story_info = JSON.parse Net::HTTP.get url
 
 			puts "Story title : #{story_info['title']}"
 
 
 			#load the json config for creating new app
-			story_config = JSON.load File.open 'story.json'
-			story_config['language'] = story_info['info']['language']
+			@story_config = JSON.load File.open 'story.json'
+			@story_config['language'] = story_info['info']['language']
 
-			story_config['name'] = "#{story_info['title']} - Interactive Story"
-			story_config['title'] = "#{story_info['title']}"
-			story_config['sku'] =  "Mango_#{story_id}"
-			story_config['bundle_id'] = "com.mangostory.#{story_id}"
-			story_config['app_rating_all'] = 0
+			@story_config['name'] = "#{story_info['title']} - Interactive Story"
+			@story_config['title'] = "#{story_info['title']}"
+			@story_config['sku'] =  "Mango_#{@story_id}"
+			@story_config['bundle_id'] = "com.mangostory.#{@story_id}"
+			@story_config['app_rating_all'] = 0
 			require 'date'
-			story_config['availability_date'] = Date.today.strftime('%b %d %Y')
-			story_config['description'] = story_info['synopsis']
+			@story_config['availability_date'] = Date.today.strftime('%b %d %Y')
+			@story_config['description'] = story_info['synopsis']
 			keywords = []
 			keywords << story_info['info']['language']
 			keywords << story_info['info']['categories']
 			keywords << story_info['info']['tags']
 			keywords << story_info['info']['subjects']
 			keywords << story_info['info']['publisher']
-			story_config['keywords'] = keywords.select {|x| x!=''}.flatten.compact
-			temp_keys = story_config['keywords'].join(',')[0..99]
+			@story_config['keywords'] = keywords.select {|x| x!=''}.flatten.compact
+			temp_keys = @story_config['keywords'].join(',')[0..99]
 			temp_splitted = temp_keys.split(',')
 			#check if the integrity of the last keyword after taking the 100 characters by checking it against the keyword list
 			unless keywords.include? temp_splitted.last
 				#If its not there, assume the last keyword is partial. And remove it
 				temp_splitted.pop
 			end
-			story_config['keywords'] = temp_splitted
-			story_config['user_name'] = user_name
-			story_config['password'] = password
-			story_config['id_type'] = 'explicit'
-			File.write 'story.json',story_config.to_json
-			story_config
+			@story_config['keywords'] = temp_splitted
+			@story_config['user_name'] = @user_name
+			@story_config['password'] = @password
+			@story_config['id_type'] = 'explicit'
+			File.write 'story.json',@story_config.to_json
+			@story_config
 		end
 
 		def download_story
@@ -106,7 +105,7 @@ require 'FileUtils'
 
 			File.open("MangoStory.zip", "wb") do |saved_file|
 			  # the following "open" is provided by open-uri
-			  open("http://api.mangoreader.com/api/v2/livestories/#{story_id}/zipped?auth_token=#{auth_token}&email=rameshvel@gmail.com", "rb") do |read_file|
+			  open("http://api.mangoreader.com/api/v2/livestories/#{@story_id}/zipped?auth_token=#{auth_token}&email=rameshvel@gmail.com", "rb") do |read_file|
 			    saved_file.write(read_file.read)
 			  end
 			end
@@ -186,7 +185,7 @@ require 'FileUtils'
 				info.version = 1.0
 				info.display_name = story_info['title']
 				#info.identifier = "com.mangostory.#{story_info['id']}" #uniqe story id at last
-				info.identifier = "com.mangostory.#{story_id}"
+				info.identifier = "com.mangostory.#{@story_id}"
 			end
 
 
@@ -202,67 +201,47 @@ require 'FileUtils'
 		end
 
 		def verify_and_distribute
-			# Create the app 
-			if app_creation == 'yes'
-				system('casperjs ghost.js --mode=app')
-			end
 
-			if app_upload == 'yes'
-				iTMSTransporter = '/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/MacOS/itms/bin/iTMSTransporter'
-				# call to get the vendor meta data xml
-				system("'#{iTMSTransporter}' -u #{user_name} -p #{password} -v off -m  lookupMetadata -vendor_id #{story_id}  -destination /tmp/#{story_id} ")
-				
-				# Get the apple id of the newly created app 
-				apple_id =`cat /tmp/#{story_id}/#{story_id}.itmsp/metadata.xml | grep apple-id | cut -d '>' -f2 | cut -d '<' -f1`.strip
+			iTMSTransporter = '/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/MacOS/itms/bin/iTMSTransporter'
+			# call to get the vendor meta data xml
+			system("'#{iTMSTransporter}' -u #{@user_name} -p #{@password} -v off -m  lookupMetadata -vendor_id Mango_#{@story_id}  -destination /tmp/#{@story_id} ")
+
+			# Get the apple id of the newly created app
+			# apple_id =`cat /tmp/#{@story_id}/#{@story_id}.itmsp/metadata.xml | grep apple-id | cut -d '>' -f2 | cut -d '<' -f1`.strip
 
 
-				# copy the release ipa to app uploads folder
-				ipa_file = 'MangoReader-Release-1.0.ipa'
-				File.cp("Build/Products/Release-iphoneos/#{ipa_file}","/tmp/#{story_id}/#{story_id}.itmsp")
+			# copy the release ipa to app uploads folder
+			ipa_file = 'MangoReader-Release-1.0.ipa'
+			FileUtils.cp("Build/Products/Release-iphoneos/#{ipa_file}","/tmp/#{@story_id}/Mango_#{@story_id}.itmsp/")
 
-				# verify the app
-				system("'#{iTMSTransporter}' -m verify -f /tmp/#{story_id}/ -u #{user_name} -p #{password}")
-
-
-			
-				ipa_size = File.size "/tmp/#{story_id}/#{ipa_file}"
-				ipa_checksum = Digest::MD5.file("/tmp/#{story_id}/#{ipa_file}").hexdigest
-
-				asset_config = " <software_assets>
-					            <asset type='bundle'>
-					                <data_file>
-					                    <size>
-					                        #{ipa_size}
-					                    </size>
-					                    <file_name>
-					                        #{ipa_file}
-					                    </file_name>
-					                    <checksum type='md5'>
-					                        #{ipa_checksum}
-					                    </checksum>
-					                </data_file>
-					            </asset>
-					        </software_assets>"
-
-				doc=Nokogiri::XML(open("/tmp/#{story_id}/#{story_id}.itmsp/metadata.xml"))
-				software_section = doc.at_css "software_metadata"
-				asset_node=Nokogiri::XML::Node.new asset_config,doc
-				software_section << asset_node
-
-				FileUtils.mkdir_p "/tmp/#{story_id}/#{story_id}.itmsp"
-				File.write("/tmp/#{story_id}/#{story_id}.itmsp/metadata.xml",doc.to_xml)
-
-				# Create success & failue & log folders
-				FileUtils.mkdir_p "/tmp/#{story_id}/success"
-				FileUtils.mkdir_p "/tmp/#{story_id}/failure"
-				FileUtils.mkdir_p "/tmp/#{story_id}/log/errors"
-
-				# Upload the ipa
-
-				system("'#{iTMSTransporter}' -u #{user_name} -p #{password} -m upload -v critical -f /tmp/#{story_id} -success /tmp/#{story_id}/success -failure /tmp/#{story_id}/failure -errorLogs /tmp/#{story_id}/log/errors -loghistory /tmp/#{story_id}/log/itms.log")
+			# verify the app
+			system("'#{iTMSTransporter}' -m verify -f /tmp/#{@story_id}/ -u #{@user_name} -p #{@password}")
 
 
-			end
+
+			ipa_size = File.size "/tmp/#{@story_id}/Mango_#{@story_id}.itmsp/#{ipa_file}"
+			ipa_checksum = Digest::MD5.file("/tmp/#{@story_id}/Mango_#{@story_id}.itmsp/#{ipa_file}").hexdigest
+
+			asset_config = " <software_assets><asset type='bundle'><data_file><size>#{ipa_size}</size><file_name>#{ipa_file}</file_name><checksum type='md5'>#{ipa_checksum}</checksum></data_file></asset></software_assets>"
+
+			doc=Nokogiri::XML(open("/tmp/#{@story_id}/Mango_#{@story_id}.itmsp/metadata.xml"))
+			software_section = doc.at_css "software_metadata"
+			asset_node=Nokogiri::XML::Node.new asset_config,doc
+			software_section << asset_node
+
+			FileUtils.mkdir_p "/tmp/#{@story_id}/Mango_#{@story_id}.itmsp"
+			File.write("/tmp/#{@story_id}/Mango_#{@story_id}.itmsp/metadata.xml",doc.to_xml)
+
+			# Create success & failue & log folders
+			FileUtils.mkdir_p "/tmp/#{@story_id}/success"
+			FileUtils.mkdir_p "/tmp/#{@story_id}/failure"
+			FileUtils.mkdir_p "/tmp/#{@story_id}/log/errors"
+
+			# Upload the ipa
+
+			system("'#{iTMSTransporter}' -u #{@user_name} -p #{@password} -m upload -v critical -f /tmp/#{@story_id} -success /tmp/#{@story_id}/success -failure /tmp/#{@story_id}/failure -errorLogs /tmp/#{@story_id}/log/errors -loghistory /tmp/#{@story_id}/log/itms.log")
+
+
 		end
 
 	    puts "Starting story builder"
@@ -280,16 +259,16 @@ require 'FileUtils'
 
 		story_ids.each do |storyid|
 
-			story_id = storyid
+			@story_id = storyid
 
-			if just_distribute == "yes"
+			if @just_distribute == "yes"
 				verify_and_distribute
 			else
-				story_config = create_json story_id
-				download_story story_id
+				@story_config = create_json
+				download_story
 
 				# Create the profile & download provisioing profile to be used in xcode build
-				if app_creation == 'yes'
+				if @app_creation == 'yes'
 					 system('casperjs ghost.js --mode=profile')
 				end
 			    
@@ -298,6 +277,11 @@ require 'FileUtils'
 				#TODO copy the game screen shots as well
 
 				build_app
+
+				# Create the app
+				if @app_creation == 'yes'
+					system('casperjs ghost.js --mode=app')
+				end
 
 				verify_and_distribute
 			end
